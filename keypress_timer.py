94b588adcd73548713bec33da36d986b3da11181
tjpaakkunainen <tomi.paakkunainen@outlook.com>
2023-07-14 12:52:30 +0300

    haha

--- keypress_timer.py
+++ keypress_timer.py
@@ -1,4 +1,4 @@
-from pynput import keyboard 
+""" from pynput import keyboard 
 import time
 
 # WIP!!!!
@@ -26,4 +26,27 @@ def main():
 x = main()
 print(x)
 
-# WIP !!!
+# WIP !!! """
+
+
+from pynput import keyboard 
+import time
+
+def on_key_release(key): #what to do on key-release
+    time_taken = round(time.time() - t, 2) #rounding the long decimal float
+    print("The key",key," is pressed for",time_taken,'seconds')
+    return False #stop detecting more key-releases
+
+def on_key_press(key): #what to do on key-press
+    return False #stop detecting more key-presses
+
+with keyboard.Listener(on_press = on_key_press) as press_listener: #setting code for listening key-press
+    press_listener.join()
+
+t = time.time() #reading time in sec
+
+with keyboard.Listener(on_release = on_key_release) as release_listener: #setting code for listening key-release
+    release_listener.join()
+
+if __name__ == "__main__":
+    
