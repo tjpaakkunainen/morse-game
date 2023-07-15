f0722f1ff8b4e4fabce90e4c9d47007b05362953
tjpaakkunainen <tomi.paakkunainen@outlook.com>
2023-07-15 19:25:34 +0300

    Clean up the game a bit and use more common morse chars
    This is very much a hacked together game with some shortcomings
    that seriously bug me, such as why I cannot come up with a better way
    than use a cleared (= empty) list to assert that round is finished.
    Also I have no idea whether the threads as used sensibly, hopefully
    will find out someday soon.
    
    Still I find some value in it as I'm memorising the morse chars better
    and better. Speaking of which, I'm only including alphanumerics for
    now because the special characters were just too much.

--- keypress_timer.py
+++ keypress_timer.py
@@ -1,6 +1,6 @@
 from pynput import keyboard
 from threading import Timer, Event
-from resource import MORSE_ALL as morse_all_chars
+from resource import MORSE_COMMON as morse_characters
 import random
 import time
 import sys
@@ -25,13 +25,13 @@ def play():
     def parse_char(input_chars: list, char_to_be_guessed: str):
  
         cleaned = "".join(input_chars)
-        ch_index = list(morse_all_chars.values()).index(char_to_be_guessed)
-        right_choice = list(enumerate(morse_all_chars.keys()))[ch_index][1]
+        ch_index = list(morse_characters.values()).index(char_to_be_guessed)
+        right_choice = list(enumerate(morse_characters.keys()))[ch_index][1]
         print()
         try:
-            print(f"Your guess: {cleaned} which corresponds to: {morse_all_chars[cleaned]}")
+            print(f"Your guess: {cleaned} which corresponds to: {morse_characters[cleaned]}")
 
-            if morse_all_chars[cleaned] == char_to_be_guessed:
+            if morse_characters[cleaned] == char_to_be_guessed:
                 print("Correct!", end=" ")
 
         except KeyError:
@@ -41,16 +41,16 @@ def play():
             print(f"Right answer: {right_choice} ({char_to_be_guessed})")
             game_active.clear()
             input_chars.clear()
-            time.sleep(1)
-            print("press any key to continue")
+            time.sleep(0.5)
+            print("\nPress enter to continue, ctrl+c to quit")
+            time.sleep(0.5)
             sys.exit()
-
 
     input_round = None
     input_chars = []
-    game_active = ["yes"]
+    game_active = ["I can 'clear' a list to get False, but can't set variable to False, WHY"]
 
-    char_to_be_guessed = random.choice(list(morse_all_chars.values()))
+    char_to_be_guessed = random.choice(list(morse_characters.values()))
     print("What's this in morse: ", char_to_be_guessed)
 
     while True:
@@ -70,14 +70,20 @@ def play():
         input_round.start()
 
 def main():
+    print("\nWelcome to morse game!\n\nYour spacebar is the key. Program gives you a character and you need to translate it to "
+          "morse.\nShort press is dit ('.') and longer press is dah ('-'). Good luck! \n")
+    first_time = True
     while True:
-        whatdo = input("wanna play? ")
-        if "y" in whatdo:
-            play()
+        if first_time:
+            whatdo = input("Press enter to play, type y to quit: \n")
         else:
-            print("you commanded:", whatdo)
-            print("quit!")
+            whatdo = input()
+        if "y" in whatdo:
+            print("\nBye!")
             break
+        else:
+            play()
+        first_time = False
 
 if __name__ == "__main__":
     main()
